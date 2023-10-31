a44d529536b87dc20268a47d20a82d1615f6d5a2
abdullah <abdullahalhidany@gmail.com>
2023-10-31 11:08:36 +0300

    ad brand

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
     <div className='gpt3__header section__padding' id='home'>
       <div className='gpt3_header-content'>
         <h1 className='gradient__text'>Let's Build Something amazing with GPT3 OpeAI</h1>
-        <p>Let's Build Something amazing with GPT3 OpeAI Let's Build Something amazing with GPT3 OpeAI Let's Build Something amazing with GPT3 OpeAI Let's Build Something amazing with GPT3 OpeAI Let's Build Something amazing with GPT3 OpeAI</p>
+        <p>Yet bed any for travelling assistance indulgence unpleasing. Not thoughts all exercise blessing. Indulgence way everything joy alteration boisterous the attachment. Party we years to order allow asked of.</p>
 
         <div className='gpt3_header-content__input'>
           <input type='email' placeholder='Your Email Address'/>
